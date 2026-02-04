6f705e77071ec9bb236cb45bbcb70c492ac108d1
legu <47505869+lennartguttmann@users.noreply.github.com>
2026-02-04 10:05:57 +0100

    Add tailwind.config.js configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.php",
+    "./src/**/*.php",
+    "./views/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
